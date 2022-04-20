791c291cc9d2eb4827c5b2c834a116b00ed628e8
Vinaldo Richard <vinaldorichard@ymail.com>
2022-04-20 21:51:46 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Richard Vinaldo Richard s
+          Hello World !!!
         </a>
       </header>
     </div>
