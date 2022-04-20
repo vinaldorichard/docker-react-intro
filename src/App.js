2df96a778d79d888ef510aab6d844577820b0042
Vinaldo Richard <vinaldorichard@ymail.com>
2022-04-20 21:57:03 +0700

    fix failed test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World !!!
+          Learn React with Richard Vinaldo Richard
         </a>
       </header>
     </div>
